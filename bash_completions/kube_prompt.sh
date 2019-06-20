07c20bdc4adc7839b039ac57aa5ac4b4724d27b8
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2019-06-20 18:26:35 +0100

    Add bash completion to the kube prompt

--- bash_completions/kube_prompt.sh
+++ bash_completions/kube_prompt.sh
@@ -6,6 +6,6 @@ __kube_ps1()
     CONTEXT=$(cat ~/.kube/config | grep "current-context:" | sed "s/current-context: //")
 
     if [ -n "$CONTEXT" ]; then
-        echo "(${CONTEXT})"
+        echo "(${CONTEXT%%.*})"
     fi
 }
