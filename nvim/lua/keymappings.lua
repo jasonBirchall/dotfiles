6b7f9a6ecf57116555cb0680ec3f4a04832625fe
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-07-16 21:26:12 +0100

    Add split and terminal navigation

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -7,3 +7,15 @@ utils.map('n', '<C-h> <C-w>', 'h')
 utils.map('n', '<C-j> <C-w>', 'j')
 utils.map('n', '<C-k> <C-w>', 'k')
 utils.map('n', '<C-w> <C-w>', 'l')
+
+-- Terminal navigation
+utils.map('t', '<C-h> <C-W>', 'h')
+utils.map('t', '<C-j> <C-W>', 'j')
+utils.map('t', '<C-k> <C-W>', 'k')
+utils.map('t', '<C-w> <C-W>', 'l')
+utils.map('n', '<leader>t', ':terminal<CR>')
+
+-- Split
+utils.map('n', '<leader>v', ':vsplit<CR>')
+utils.map('n', '<leader>s', ':split<CR>')
+utils.map('n', '<leader>V', ':vertical resize 350<CR>')
