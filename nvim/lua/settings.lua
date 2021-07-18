edf5a785597864e4c08c881b9d720b34b9df6d5b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-07-18 10:30:45 +0100

    Copy general settings over from init.vim

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -3,14 +3,19 @@ local utils = require('utils')
 local cmd = vim.cmd
 local indent = 4
 
+-- General
 cmd 'syntax enable'
 cmd 'filetype plugin indent on'
-utils.opt('b', 'expandtab', true)
+utils.opt('b', 'expandtab', true) -- convert tabs to spaces
 utils.opt('b', 'shiftwidth', indent)
+utils.opt('o', 'smarttab', true)
 utils.opt('b', 'smartindent', true)
 utils.opt('b', 'tabstop', indent)
+utils.opt('o', 'showmatch', true) -- show matching bracket end
+utils.opt('o', 'clipboard','unnamed,unnamedplus')
 utils.opt('o', 'hidden', true)
 utils.opt('o', 'ignorecase', true)
+utils.opt('o', 'mouse', 'a')
 utils.opt('o', 'scrolloff', 4 )
 utils.opt('o', 'shiftround', true)
 utils.opt('o', 'smartcase', true)
@@ -18,7 +23,18 @@ utils.opt('o', 'splitbelow', true)
 utils.opt('o', 'splitright', true)
 utils.opt('o', 'wildmode', 'list:longest')
 utils.opt('w', 'number', true)
-utils.opt('o', 'clipboard','unnamed,unnamedplus')
+utils.opt('w', 'wrap', true)
+utils.opt('o', 'ruler', true)
+utils.opt('o', 'encoding', 'UTF-8')
 
 -- Highlight on yank
-vim.cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
+cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
+
+-- Commenting
+cmd 'autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o'
+
+-- Filetype
+cmd 'filetype plugin indent on'
+
+-- Backups
+-- utils.opt('b', 'nobackup', true)
