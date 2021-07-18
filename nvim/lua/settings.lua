fb25d4de5b31cba083109e18c3053f87bac15d60
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-07-18 20:23:05 +0100

    Sort general nvim settings

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -4,30 +4,29 @@ local cmd = vim.cmd
 local indent = 4
 
 -- General
-cmd 'syntax enable'
 cmd 'filetype plugin indent on'
+cmd 'syntax enable'
 utils.opt('b', 'expandtab', true) -- convert tabs to spaces
 utils.opt('b', 'shiftwidth', indent)
-utils.opt('o', 'smarttab', true)
 utils.opt('b', 'smartindent', true)
 utils.opt('b', 'tabstop', indent)
-utils.opt('o', 'showmatch', true) -- show matching bracket end
-utils.opt('o', 'termguicolors', true) -- show matching bracket end
 utils.opt('o', 'clipboard','unnamed,unnamedplus')
+utils.opt('o', 'cmdheight', 1)
+utils.opt('o', 'encoding', 'UTF-8')
 utils.opt('o', 'hidden', true)
 utils.opt('o', 'ignorecase', true)
 utils.opt('o', 'mouse', 'a')
+utils.opt('o', 'ruler', true)
 utils.opt('o', 'scrolloff', 4 )
 utils.opt('o', 'shiftround', true)
+utils.opt('o', 'showmatch', true) -- show matching bracket end
 utils.opt('o', 'smartcase', true)
+utils.opt('o', 'smarttab', true)
 utils.opt('o', 'splitbelow', true)
 utils.opt('o', 'splitright', true)
 utils.opt('o', 'wildmode', 'list:longest')
 utils.opt('w', 'number', true)
 utils.opt('w', 'wrap', true)
-utils.opt('o', 'ruler', true)
-utils.opt('o', 'encoding', 'UTF-8')
-utils.opt('o', 'cmdheight', 1)
 
 -- Highlight on yank
 cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
