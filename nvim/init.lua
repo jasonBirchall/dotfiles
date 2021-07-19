5830b07444dc3cacc1a86277d476f301722f05bd
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-07-19 13:32:33 +0100

    Handle the git clone in an if condition and use start dir rather than opt

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,11 +5,13 @@ local fn = vim.fn
 local execute = vim.api.nvim_command
 
 -- Auto install packer.nvim if not exists
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
 end
-vim.cmd [[packadd packer.nvim]]
+
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
 
 -- Sensible defaults
