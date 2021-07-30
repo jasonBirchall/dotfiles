8305fa155f6548241078494149d2f5d9c1e422c7
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-07-30 16:24:12 +0100

    Add keybindings for git shortcuts

--- nvim/lua/config/fugitive.lua
+++ nvim/lua/config/fugitive.lua
@@ -1,2 +1,5 @@
 local utils = require('utils')
 utils.map('n', '<Leader>gs', '<cmd>Gstatus<CR>')  -- Git status
+utils.map('n', '<Leader>gv', '<cmd>Gvdiffsplit<CR>')  -- Git diff split vertically
+utils.map('n', '<Leader>gb', '<cmd>GBrowse<CR>')  -- View git commit in web browser
+utils.map('n', '<Leader>gc', '<cmd>Gcommit<CR>')  -- Commit stage
