f8e0c774afcf99ab1c74fa5ac1c508af46dd6712
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-08-12 14:19:28 +0100

    Add hidden files for Telescope file_browser

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -5,11 +5,9 @@ require("telescope").setup {
     -- Your special builtin config goes in here
     buffers = {
       sort_lastused = true,
-      theme = "dropdown",
       previewer = true,
     },
     find_files = {
-      theme = "dropdown"
     }
   },
 }
