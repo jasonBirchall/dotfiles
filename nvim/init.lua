5db6318bf8a9f275a8dda5625af303d1986bab76
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-08-17 21:02:27 +0100

    Handle packer install in init script

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,14 +3,24 @@ vim.g.mapleader = ' '
 
 local fn = vim.fn
 local execute = vim.api.nvim_command
+local vim = vim
 
--- Auto install packer.nvim if not exists
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
 end
+
+vim.cmd('packadd packer.nvim')
+
+local packer = require'packer'
+local util = require'packer.util'
+
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
 
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
 
