ddde1bd73b235299defa1f20a6dbdbdf3665b328
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-08-31 15:32:17 +0100

    Make geogit the primary git tool in nvim

--- nvim/lua/config/fugitive.lua
+++ nvim/lua/config/fugitive.lua
@@ -1,6 +1,3 @@
 local utils = require('utils')
-utils.map('n', '<Leader>gs', '<cmd>Gstatus<CR>')  -- Git status
-utils.map('n', '<Leader>gv', '<cmd>Gvdiffsplit<CR>')  -- Git diff split vertically
 utils.map('n', '<Leader>gb', '<cmd>GBrowse<CR>')  -- View git commit in web browser
-utils.map('n', '<Leader>gc', '<cmd>Gcommit<CR>')  -- Commit stage
 utils.map('n', '<Leader>gp', '<cmd>Gpush<CR>')  -- Push changes
--- nvim/lua/config/neogit.lua
+++ nvim/lua/config/neogit.lua
@@ -1,3 +1,4 @@
 local utils = require('utils')
 
-utils.map('n', '<Leader>gn', '<cmd>Neogit kind=split<CR>')
+utils.map('n', '<Leader>gs', '<cmd>Neogit kind=split<CR>')
+utils.map('n', '<Leader>gc', '<cmd>Neogit commit kind=split<CR>')
