69a5e7212fc86819c34c9ca5eb600332247d5a1a
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-03-18 15:15:33 +0000

    Add indent-blankline config

--- nvim/lua/config/indent-blankline.lua
+++ nvim/lua/config/indent-blankline.lua
@@ -1,1 +1,6 @@
-require("packer").startup()
+require("indent_blankline").setup {
+  -- for example, context is off by default, use this to turn it on
+  -- show_current_context = true,
+  show_current_context_start = true,
+  space_char_blankline = " ",
+}
