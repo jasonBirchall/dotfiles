a35f15cf19f29254a0305109203a42f8d4383831
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-03-29 11:24:45 +0100

    Add toggle for method/function signature show

--- nvim/lua/config/signature.lua
+++ nvim/lua/config/signature.lua
@@ -45,7 +45,7 @@ require ("lsp_signature").setup({
   shadow_blend = 36, -- if you using shadow as border use this set the opacity
   shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
   timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-  toggle_key = nil -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+  toggle_key = '<M-t>' -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 })
 
 require("lsp_signature").status_line(max_width)
