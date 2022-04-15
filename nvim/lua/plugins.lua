62e0351b92b0ce04d0ebd987c728953082befbcc
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-04-15 20:41:07 +0100

    feat(nvim): add yaml completion
    also include additional plugins like spell, emoji etc

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -59,8 +59,18 @@ return require('packer').startup(function()
   use { 'neovim/nvim-lspconfig' }
 
   use { 'ray-x/lsp_signature.nvim' }
-  use { 'hrsh7th/nvim-cmp' }
-  use { 'hrsh7th/cmp-nvim-lsp' } -- LSP source for nvim-cmp
+  use{
+	   'hrsh7th/nvim-cmp',
+		requires = {
+		  'hrsh7th/cmp-nvim-lsp',
+		  'hrsh7th/cmp-buffer',
+		  'hrsh7th/cmp-path',
+		  'hrsh7th/cmp-cmdline',
+		  'f3fora/cmp-spell',
+		  'hrsh7th/cmp-calc',
+		  'lukas-reineke/cmp-rg'
+	 },
+  }
   use { 'github/copilot.vim' }
   use { 'saadparwaiz1/cmp_luasnip' } -- Snippets source for nvim-cmp
   use { 'L3MON4D3/LuaSnip' } -- Snippets plugin
