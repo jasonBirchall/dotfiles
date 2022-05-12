27dea724fedf2ab67368bf1f741d3f2430137513
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-05-12 22:01:45 +0100

    feat(nvim): change toggle keybinding

--- nvim/lua/config/signature.lua
+++ nvim/lua/config/signature.lua
@@ -45,7 +45,7 @@ require ("lsp_signature").setup({
   shadow_blend = 36, -- if you using shadow as border use this set the opacity
   shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
   timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-  toggle_key = '<M-x>' -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+  toggle_key = '<C-x>' -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 })
 
 -- recommended:
