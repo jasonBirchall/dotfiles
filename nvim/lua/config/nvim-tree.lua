a9e310d9597f66ba850cdfbad71a17601c37815f
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-09-01 16:53:27 +0100

    :fire: Remove neovim-tree mappings

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -14,12 +14,6 @@ require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
 		number = false,
 		relativenumber = false,
 		signcolumn = "yes",
-		mappings = {
-			custom_only = false,
-			list = {
-				-- user mappings go here
-			},
-		},
 	},
 	hijack_directories = {
 		enable = true,
