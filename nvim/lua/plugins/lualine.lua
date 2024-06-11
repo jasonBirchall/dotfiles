2c19d74e4bac20e01fde704f00f9ab448165ade9
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-06-11 16:08:47 +0100

    :wrench: Add copilot lualline config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,39 @@
 
 return {
-  -- the opts function can also be used to change the default opts:
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
-    end,
-  },
+  "nvim-lualine/lualine.nvim",
+  optional = true,
+  event = "VeryLazy",
+  opts = function(_, opts)
+    local colors = {
+      [""] = LazyVim.ui.fg("Special"),
+      ["Normal"] = LazyVim.ui.fg("Special"),
+      ["Warning"] = LazyVim.ui.fg("DiagnosticError"),
+      ["InProgress"] = LazyVim.ui.fg("DiagnosticWarn"),
+    }
+    table.insert(opts.sections.lualine_x, 2, {
+      function()
+        local icon = require("lazyvim.config").icons.kinds.Copilot
+        local status = require("copilot.api").status.data
+        return icon .. (status.message or "")
+      end,
+      cond = function()
+        if not package.loaded["copilot"] then
+          return
+        end
+        local ok, clients = pcall(LazyVim.lsp.get_clients, { name = "copilot", bufnr = 0 })
+        if not ok then
+          return false
+        end
+        return ok and #clients > 0
+      end,
+      color = function()
+        if not package.loaded["copilot"] then
+          return
+        end
+        local status = require("copilot.api").status.data
+        return colors[status.status] or colors[""]
+      end,
+    })
+  end,
 }
+
