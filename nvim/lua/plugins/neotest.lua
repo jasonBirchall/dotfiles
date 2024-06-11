0beaa8be76401e481d733bb48bb2d2e96ed1bd4b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-06-11 16:44:05 +0100

    :wrench: Neotest Python configuration setup

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -7,17 +7,13 @@ return {
   {
     "nvim-neotest/neotest",
     opts = {
-      -- Can be a list of adapters like what neotest expects,
-      -- or a list of adapter names,
-      -- or a table of adapter names, mapped to adapter configs.
-      -- The adapter will then be automatically loaded with the config.
-      adapters = {},
-      -- Example for loading neotest-go with a custom config
-      -- adapters = {
-      --   ["neotest-go"] = {
-      --     args = { "-tags=integration" },
-      --   },
-      -- },
+      adapters = {
+        ["neotest-python"] = {
+          -- Here you can specify the settings for the adapter, i.e.
+          -- runner = "pytest",
+          -- python = ".venv/bin/python",
+        },
+    },
       status = { virtual_text = true },
       output = { open_on_run = true },
       quickfix = {
