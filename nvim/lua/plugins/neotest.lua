fd279fcbdca8e0671c40a47bd547f74e2bd8d928
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-07-01 16:59:36 +0100

    :wrench: Add router and change neotest config

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,13 +1,26 @@
 return {
-  { "nvim-neotest/neotest-python" },
   {
     "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-plenary",
+    },
     opts = {
       adapters = {
+        ["neotest-plenary"] = {},
         ["neotest-python"] = {
           -- Here you can specify the settings for the adapter, i.e.
-          -- runner = "unittest",
-          -- python = ".venv/bin/python",
+          dap = {
+            console = "integratedTerminal",
+            stopOnEntry = false,  -- which is the default(false)
+            subProcess = false,  -- see config/testing.lua
+            openUIOnEntry = false,
+            justMyCode = false,
+          },
+          runner = "pytest",
+          args = { "-vv", "-s" },
+          -- args = { "--log-level", "DEBUG" },
+          -- python = vim.g.python_host_prog,
         },
       },
     },
