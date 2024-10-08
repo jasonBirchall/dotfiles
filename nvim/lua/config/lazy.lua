0814b1eed73b6d0d6532dabb02a484f8a151406b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-10-08 19:49:33 +0100

    :wrench: Add recommended plugin config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
