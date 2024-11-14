7e19659762942ed2eadcaae92d7eec13a298bbd8
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-11-14 15:23:31 +0000

    :fire: Remove edgy as it's failing to load

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.vscode" },
