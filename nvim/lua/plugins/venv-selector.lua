6556ad872385b66b049d0106caf5f0ee9c8adb8c
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-12-16 15:19:44 +0000

    :fire: Purge a large number of plugins

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -1,5 +1,6 @@
 return {
   "linux-cultist/venv-selector.nvim",
+  dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
   opts = {
     settings = {
       options = {
