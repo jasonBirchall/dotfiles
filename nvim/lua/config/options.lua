33f0ec101540c79605bb10a98f4b75b81f00de3a
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2026-01-02 23:44:14 +0000

    ♻️ Add python changes

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -47,3 +47,8 @@ vim.g.deprecation_warnings = false
 -- You can disable this for a buffer by setting `vim.b.trouble_lualine = false`
 vim.g.trouble_lualine = true
 
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
---
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,31 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ruff = {
+        cmd_env = { RUFF_TRACE = "messages" },
+        init_options = {
+          settings = {
+            logLevel = "error",
+          },
+        },
+        keys = {
+          {
+            "<leader>co",
+            LazyVim.lsp.action["source.organizeImports"],
+            desc = "Organize Imports",
+          },
+        },
+      },
+      ruff_lsp = {
+        keys = {
+          {
+            "<leader>co",
+            LazyVim.lsp.action["source.organizeImports"],
+            desc = "Organize Imports",
+          },
+        },
+      },
+    },
+  }
+}
