bcca3e41fccfb76cda7fd904c40e22ed4ff0b9f8
json0 <jason.birchall@pm.me>
2026-02-22 22:10:42 +0000

    Fix up some macos links and include nvim config

--- home.nix
+++ home.nix
@@ -17,6 +17,8 @@
     jq
     htop
     tree
+    gcc
+    gnumake
 
     # --- Git & Version Control ---
     git
@@ -33,6 +35,7 @@
     # --- Development & Infrastructure ---
     go
     python3
+    uv
     nodejs
     kubectl
     k9s
@@ -44,13 +47,27 @@
   home.file = {
     # Tmux
     ".tmux.conf".source = ./tmux/tmux.conf;
-    
+    # Layer 2: Configuration Files & Directories
+
+    # Neovim (LazyVim) - Out-of-store symlink so it can write to lazy-lock.json
+    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/workarea/dotfiles/nvim";
+
     # Zsh Shell
     ".config/zsh/.zshenv".source = ./zsh/zshenv;
     ".config/zsh/.zshrc".source = ./zsh/zshrc;
     
     # Ranger
     ".config/ranger/rc.conf".source = ./ranger/rc.conf;
+
+    # Custom scripts
+    "bin/connection-checker.py".source = ./bin/connection-checker/connection-checker.py;
+  };
+
+  # Set default environment variables
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    GIT_EDITOR = "nvim";
   };
   # Let Home Manager manage itself
   programs.home-manager.enable = true;
